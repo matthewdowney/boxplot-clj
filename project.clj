eabe3fbb78911ae21f22703434232dc0494a98ef
Matthew Downey <matthewdowney20@gmail.com>
2018-07-31 20:59:42 -0500

    Initial commit

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject boxplot-clj "0.1.0"
+  :description "Plot a box plot as a string."
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [com.stuartsierra/frequencies "0.1.0"]])
+
